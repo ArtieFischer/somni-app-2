04235c07fb413f96498a3b9e64f26ed99625fa98
ArtieFischer <olejniczak.grzeg@gmail.com>
2025-06-10 20:16:08 +0530

    Updated OfflineQueueTest.tsx

--- apps/mobile/src/components/test/OfflineQueueTest.tsx
+++ apps/mobile/src/components/test/OfflineQueueTest.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity, ScrollView, Alert } from 'react-native';
-import { useOfflineRecordingQueue } from '@somni/hooks'; // Import the hook
+import { useOfflineRecordingQueue } from '../../hooks/useOfflineRecordingQueue.ts'; // Import the hook
 import { OfflineRecording } from '@somni/types';
 
 const TestButton: React.FC<{
